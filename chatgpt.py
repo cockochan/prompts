233d1f79d1f22094313c38437283ce7d80fe9ac6
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2024-01-23 11:22:56 +0000

    retrieval

--- chatgpt.py
+++ chatgpt.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+import lists
 import openai
 from langchain.chains import ConversationalRetrievalChain, RetrievalQA
 from langchain.chat_models import ChatOpenAI
@@ -10,9 +10,8 @@ from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.llms import OpenAI
 from langchain.vectorstores import Chroma
-
 import constants
-
+import json
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
 # Enable to save to disk & reuse the model (for repeated queries on the same data)
@@ -27,8 +26,9 @@ if PERSIST and os.path.exists("persist"):
   vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
   index = VectorStoreIndexWrapper(vectorstore=vectorstore)
 else:
-  #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
-  loader = DirectoryLoader("data/")
+  loader = TextLoader("data/report.txt") # Use this line if you only need data.txt
+  # loader = DirectoryLoader("data/")
+  data = loader.load()
   if PERSIST:
     index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
   else:
@@ -40,13 +40,27 @@ chain = ConversationalRetrievalChain.from_llm(
 )
 
 chat_history = []
-while True:
-  if not query:
-    query = input("Prompt: ")
-  if query in ['quit', 'q', 'exit']:
-    sys.exit()
-  result = chain({"question": query, "chat_history": chat_history})
-  print(result['answer'])
+jsonfile = open('./format.json','r')
+format = json.load(jsonfile)
+prompt = f"""
+Find  any words or thir synonyms or forms from the two lists in the text ib the data folder
+, count them and provide two json formatted lists of count for top 5 words.
+just do it here. Do not mix list 1 and list 2 provide two separate jsons. Find words from both lists in text, do not provide code, answer in folowing format ```{format}```, only list top 10 occurences with word presented as json key and count of its occurences as json value:
 
-  chat_history.append((query, result['answer']))
-  query = None
+
+give me json with key stats about the text in this format with properties like:
+length, readability(estimate on scale 1-10), count of references to "RACI", "environment","TBC","out of scope","Risk","Distribution", "unit test","scrum","agile","Performance","UAT"
+
+```{data}```,```{lists.list1}```,```{lists.list2}```
+
+please test the output that it does not contain string "count" or "word" but has actual keys and values in it.
+
+# give a very short suggestion on how to improve the text including specific instructions:
+# If count of word RACI < 1 Then say "I didn't find a clear indication of who is responsible. Consider adding a responsibility matrix such as RACI (responsible, accountable, consulted, informed) to make it clearer who has to do what"
+"""
+
+result = chain({"question": prompt, "chat_history": chat_history})
+print(result['answer'])
+
+chat_history.append((query, result['answer']))
+
---
+++ lists.py
@@ -0,0 +1,9 @@
+list1 = f"""one of", "can", "could", "is like", "one off", "must", "shall", "should", "will", "would", "above", "below", "industry best practice", "such","the previous", "them", "these", "they", "this", "those", "all", "any", "appropriate", "custom", "efficient", "every", "few", "frequent", _
+"improved", "infrequent", "intuitive", "invalid", "many", "might", "most", "normal", "ordinary", "rare", "same", "seamless", "several", "similar","some", "standard", "the complete", "the entire", "transparent", "typical", "one of", "e.g.", "eg ", "i.e.", "approx", "TBC", _
+"Quality Centre", "may", "Performance Centre", "such as", "fit for purpose", "to the client's", "when required", "satisfaction", "not limited to", "time and budget", "Sogeti proposes"""
+
+list2 = f"""by and large", "common", "commonly", "custom", "customarily", "efficient", "efficiently", "frequently", "generally", "hardly ever", _
+"in general", "infrequent", "infrequently", "intuitively", "just about", "more often than not", "more or less", "mostly", "nearly", " normally", _
+"not quite", "often", "on the odd occasion", "ordinarily", "rarely", "roughly", "seamlessly", "seldom", "similarly", "sometime", "somewhat", "transparently", _
+"typically", "usually", "virtually", "adjust", "alter", "amend", "improve", "maximise", "minimise", "in addition", "likewise", "yet", "whereas", "as required", _
+"as necessary", "annually", "at a given time", "when required", "at the appropriate time", "up to", "among", "including", "etc", "etc.", "?", "brief", "ensure", "best endeavours"""
